4dfe2da78345d5cbcc2c7c4f5e3e982bbf53b6a7
Stephanie Chau <schau@uc.cl>
2020-12-19 13:50:02 -0300

    refactor(): Remove console.log

--- react-app/src/helpers/winningRules.js
+++ react-app/src/helpers/winningRules.js
@@ -1,6 +1,6 @@
 export const rowWinner = (board, index) => {
   if (!(board[index][0].value && board[index][1].value && board[index][2].value)) return false;
-  console.log(board[index][0], board[index][1], board[index][2]);
+
   return (board[index][0].value === board[index][1].value) && (board[index][1].value === board[index][2].value);
 };
 
@@ -12,10 +12,12 @@ export const colWinner = (board, index) => {
 
 export const diagonalWinner = (board) => {
   if (!(board[0][0].value && board[1][1].value && board[2][2].value)) return false;
+  
   return (board[0][0].value === board[1][1].value) && (board[1][1].value === board[2][2].value);
 };
 
 export const reverseDiagonalWinner = (board) => {
   if (!(board[2][0].value && board[1][1].value && board[0][2].value)) return false;
+  
   return (board[2][0].value === board[1][1].value) && (board[1][1].value === board[0][2].value);
 };
